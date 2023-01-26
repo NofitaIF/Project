8d565a81ca63f5175822030ae62b3e5573f23cce
Nofita Indah Fitriani <123503424+NofitaIF@users.noreply.github.com>
2023-01-26 19:45:47 +0700

    Add files via upload

---
+++ Key result divisi marketing dan operasional.sql
@@ -0,0 +1,21 @@
+
+/*Membuat query untuk divisi marketing.
+menampilkan apakah pendapatan selama 3 bulan terakhir sudah mencapai target atau belum (target : 200.000)*/
+	
+select  date_part('year',created_at) tahun, date_part('month',created_at) bulan ,round(sum(sale_price)) omset
+from order_items
+group by date_part('year',created_at), date_part('month',created_at)
+order by date_part('year',created_at) desc,date_part('month',created_at) desc
+limit 3
+
+
+/*Membuat query untuk divisi operasional.
+menampilkan apakah rata-rata waktu pemrosesan selama 3 bulan terakhir sesuai target.
+Dari pesanan dibuat sampai pesanan dikirim adalah 12 jam.*/
+
+select date_part('year',created_at) tahun,date_part('month',created_at) bulan, date_trunc('hour',(avg(shipped_at - created_at))) rata_rata_waktu_pengiriman
+from order_items
+group by date_part('year',created_at), date_part('month',created_at)
+order by date_part('year',created_at) desc, date_part('month',created_at) desc
+limit 3
+
